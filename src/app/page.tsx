4f0828e71d0d564e378c9581d0b719c8271e7701
k_95.21 <kohta0421kochi@gmail.com>
2023-11-13 18:15:24 +0900

    add useState version form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from './page.module.css'
 
+import UseStateForm from '../components/UseStateForm'
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>React Form Best Practice</h1>
+      <UseStateForm />
     </main>
   )
 }
---
+++ src/components/UseStateForm.jsx
@@ -0,0 +1,46 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+
+const UseStateForm = () => {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log({
+            email,
+            password,
+        });
+    };
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email"id='email' value={email} onChange={handleChangeEmail} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' name='password' value={password} onChange={handleChangePassword} />
+                </div>
+                <div>
+                    <button type='submit'>ログイン</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default UseStateForm
